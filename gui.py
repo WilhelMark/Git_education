582b9cb7d50fbdd867368903f7a9e0fef3b325c2
Viktor Golev <vwpiligrim@gmail.com>
2024-03-27 21:39:15 +0300

    Improvement of the graphical interface and basic functionality

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 
 class PhonebookGUI(tk.Frame):
-    def __init__(self, master, data_handler):
+    def __init__(self, master, data_handler, import_export_manager):
         super().__init__(master)
         self.master = master
         self.data_handler = data_handler
+        self.import_export_manager = import_export_manager
 
         self.create_widgets()
 
@@ -16,14 +17,31 @@ class PhonebookGUI(tk.Frame):
         self.contact_listbox = tk.Listbox(self)
         self.contact_listbox.pack()
 
-        self.add_button = tk.Button(self, text="Add Contact", command=self.add_contact)
+        self.search_entry = tk.Entry(self)
+        self.search_entry.pack()
+
+        self.search_button = tk.Button(self, text="Поиск", command=self.search_contacts)
+        self.search_button.pack()
+
+        self.add_button = tk.Button(self, text="Добавить контакт", command=self.show_add_contact_window)
         self.add_button.pack()
 
-        self.delete_button = tk.Button(self, text="Delete Contact", command=self.delete_contact)
+        self.edit_button = tk.Button(self, text="Редактировать контакт", command=self.show_edit_contact_window)
+        self.edit_button.pack()
+
+        self.delete_button = tk.Button(self, text="Удалить контакт", command=self.delete_contact)
         self.delete_button.pack()
 
-    def add_contact(self):
-        # Logic for adding a contact
+    def search_contacts(self):
+        # Logic for searching contacts
+        pass
+
+    def show_add_contact_window(self):
+        # Logic for showing add contact window
+        pass
+
+    def show_edit_contact_window(self):
+        # Logic for showing edit contact window
         pass
 
     def delete_contact(self):
